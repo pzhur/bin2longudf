4cdafee2e52e1dcbfad52c86635d321283b90dd5
Pavel Zhuravlev <pavel.zhuravlev@census.gov>
2023-02-06 16:29:05 -0500

    added NULL check

--- src/main/scala/gov/census/das/spark/udf/binaryStreamToLongArray.scala
+++ src/main/scala/gov/census/das/spark/udf/binaryStreamToLongArray.scala
@@ -8,6 +8,9 @@ import java.nio.ByteOrder
 class binaryStreamToLongArray extends UDF1[Array[Byte], Array[Long]] {
 
   override def call(binaryArray: Array[Byte]): Array[Long] = {
+    if (binaryArray == null) {
+      return null
+    }
     val bb: ByteBuffer = ByteBuffer.wrap(binaryArray)
     bb.order(ByteOrder.nativeOrder)
     val longs = new Array[Long](binaryArray.length / 8)
