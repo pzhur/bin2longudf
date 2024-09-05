dfd125c276515b0a25a74ab3c4911e68ae04c875
Pavel Zhuravlev <pavel.zhuravlev@census.gov>
2024-09-05 13:46:48 -0400

    Comments for BinaryStremtoType converting UDFs

--- src/main/scala/gov/census/das/spark/udf/binaryStreamToLongArray.scala
+++ src/main/scala/gov/census/das/spark/udf/binaryStreamToLongArray.scala
@@ -1,3 +1,8 @@
+// Implements a spark UDF that converts a byte stream (aka array of type Binary) to array of type Long
+// The use case is to save numpy arrays in pyspark as Binary spark type (using .tobytes), and then
+// use this function to convert the array into spark LongType(), so that they can be operated on by other
+// UDFs expecting arrays, or standard spark SQL functions, or save to parquet taking less space.
+
 package gov.census.das.spark.udf
 
 import org.apache.spark.sql.api.java.UDF1
